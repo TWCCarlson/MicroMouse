e35ef754e2e1b17cba18af05d90505e3ff94b418
Thomas W. C. Carlson <72635603+TWCCarlson@users.noreply.github.com>
2020-11-01 15:52:38 -0800

    Final Design

--- CODE/MicroMouse/blank/blank.ino
+++ CODE/MicroMouse/blank/blank.ino
@@ -1,22 +1,31 @@
+#define STEP_PIN2 33
+#define DIR_PIN2 15
+
 #define STEP_PIN 13
-#define DIR_PIN 12
+#define DIR_PIN 14
  
 bool dirHigh;
  
 void setup()
 {
   dirHigh = true;
-  digitalWrite(DIR_PIN, LOW);
-  digitalWrite(STEP_PIN, LOW);
-  pinMode(DIR_PIN, OUTPUT);
-  pinMode(STEP_PIN, OUTPUT);
+  //digitalWrite(DIR_PIN, LOW);
+  //digitalWrite(STEP_PIN, LOW);
+  digitalWrite(DIR_PIN2, LOW);
+  digitalWrite(STEP_PIN2, LOW);
+  //pinMode(DIR_PIN, OUTPUT);
+  //pinMode(STEP_PIN, OUTPUT);
+  pinMode(DIR_PIN2, OUTPUT);
+  pinMode(STEP_PIN2, OUTPUT);
 }
  
 void loop()
 {
     // Trigger the motor to take one step.
-    digitalWrite(STEP_PIN, HIGH);
+    //digitalWrite(STEP_PIN, HIGH);
+    digitalWrite(STEP_PIN2, HIGH);
     delay(1);
-    digitalWrite(STEP_PIN, LOW);
+    //digitalWrite(STEP_PIN, LOW);
+    digitalWrite(STEP_PIN2, LOW);
     delay(1);
 }
