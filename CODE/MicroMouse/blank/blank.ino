f6d5aa7e5e7cc8a310d254d08243833c828e0976
Thomas W. C. Carlson <72635603+TWCCarlson@users.noreply.github.com>
2020-11-25 00:13:56 -0800

    Backlog of updates
    IR Calibration done
    Chassis updates
    Still needs cleaning up

--- CODE/MicroMouse/blank/blank.ino
+++ CODE/MicroMouse/blank/blank.ino
@@ -1,31 +1,82 @@
+//LEFT STEPPER
 #define STEP_PIN2 33
 #define DIR_PIN2 15
 
-#define STEP_PIN 13
+//RIGHT STEPPER
+#define STEP_PIN 32
 #define DIR_PIN 14
+
+#define SenL A2
+#define SenC A3
+#define SenR A4
  
 bool dirHigh;
  
 void setup()
 {
   dirHigh = true;
-  //digitalWrite(DIR_PIN, LOW);
-  //digitalWrite(STEP_PIN, LOW);
-  digitalWrite(DIR_PIN2, LOW);
+  digitalWrite(DIR_PIN, LOW); //HIGH: RIGHT WHEEL FWD
+  digitalWrite(STEP_PIN, LOW);
+  digitalWrite(DIR_PIN2, HIGH); //LOW: LEFT WHEEL FWD
   digitalWrite(STEP_PIN2, LOW);
-  //pinMode(DIR_PIN, OUTPUT);
-  //pinMode(STEP_PIN, OUTPUT);
+  pinMode(DIR_PIN, OUTPUT);
+  pinMode(STEP_PIN, OUTPUT);
   pinMode(DIR_PIN2, OUTPUT);
   pinMode(STEP_PIN2, OUTPUT);
+  Serial.begin(115200);
+  
 }
  
 void loop()
 {
+    float volts1 = analogRead(SenC);
+    //float volts11 = analogRead(SenL);
+    //float volts21 = analogRead(SenR);
+    delay(1);
+    float volts2 = analogRead(SenC);
+    //float volts12 = analogRead(SenL);
+    //float volts22 = analogRead(SenR);
+    delay(1);
+    float volts3 = analogRead(SenC);
+    //float volts13 = analogRead(SenL);
+    //float volts23 = analogRead(SenR);
+    delay(1);
+    float volts4 = analogRead(SenC);
+    //float volts14 = analogRead(SenL);
+    //float volts24 = analogRead(SenR);
+    delay(1);
+    float volts5 = analogRead(SenC);
+    //float volts15 = analogRead(SenL);
+    //float volts25 = analogRead(SenR);
+    delay(1);
+    float Voltavg = ((volts1))*3.3/4095;
+    Serial.println(Voltavg);
+    //delay(100);
+
+    if (Voltavg < 1.2) {
+      // FWD
+      digitalWrite(DIR_PIN, LOW); //HIGH: RIGHT WHEEL FWD
+      digitalWrite(DIR_PIN2, HIGH); //LOW: LEFT WHEEL FWD
+      
+      digitalWrite(STEP_PIN, HIGH);
+      digitalWrite(STEP_PIN2, HIGH);
+      delay(2);
+      digitalWrite(STEP_PIN, LOW);
+      digitalWrite(STEP_PIN2, LOW);
+      delay(2);
+    }
+    if (Voltavg > 1.2) {
+      // REVERSE
+      digitalWrite(DIR_PIN, HIGH); //HIGH: RIGHT WHEEL FWD
+      digitalWrite(DIR_PIN2, LOW); //LOW: LEFT WHEEL FWD
+      
+      digitalWrite(STEP_PIN, HIGH);
+      digitalWrite(STEP_PIN2, HIGH);
+      delay(2);
+      digitalWrite(STEP_PIN, LOW);
+      digitalWrite(STEP_PIN2, LOW);
+      delay(2);
+    }
     // Trigger the motor to take one step.
-    //digitalWrite(STEP_PIN, HIGH);
-    digitalWrite(STEP_PIN2, HIGH);
-    delay(1);
-    //digitalWrite(STEP_PIN, LOW);
-    digitalWrite(STEP_PIN2, LOW);
-    delay(1);
+    
 }
---
+++ IRCalib/IRCalib.ino
@@ -0,0 +1,96 @@
+//Mode testing
+#include <ArduinoSort.h>
+float array[100];
+//Setup for button
+//Pushing the button should trigger a sampling and averaging period, then return a result
+const int buttonPin = 13;
+//const int ledPin = 13;
+int buttonState = 0;
+//Setup for IR sensor
+#define Sen A1
+float volts;
+float voltavg;
+
+void setup() {
+  // put your setup code here, to run once:
+  //Initialize the button
+  //pinMode(ledPin, OUTPUT);
+  pinMode(buttonPin, INPUT);
+  //Open the serial monitor
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  //Read the button state
+  voltavg = 0;
+  buttonState = digitalRead(buttonPin);
+  /*Print the button state
+  Serial.println(buttonState);*/
+  /*Control the onboard LED
+  //Lights up when button pressed
+  if (buttonState == HIGH){
+    digitalWrite(ledPin, HIGH);
+  } else {
+    digitalWrite(ledPin, LOW);
+  }*/
+  //If the button is pressed, sample 30 times
+  /*if (buttonState == HIGH){
+    Serial.println("Acquiring...");
+    for (int i = 1; i <= 30; i++) {
+      volts = analogRead(Sen);
+      //Serial.println("Read: ");
+      //Serial.println(volts);
+      //convert to an actual voltage reading
+      //3v3, 1023 values for UNO
+      //3v3, 4095 values for ESP32
+      volts = volts*3.3/4095;
+      //Serial.println("Voltage avg read: ");
+      //Serial.println(volts);
+      voltavg = voltavg + volts;
+      delay(10);
+    }
+    voltavg = voltavg / 30;
+    Serial.println("Sample average voltage: ");
+    Serial.println(voltavg);
+  }*/
+
+  //Using mode
+  float number = array[0];
+  float Mode = number;
+  int count = 1;
+  int countMode = 1;
+  if (buttonState == HIGH){
+    Serial.println("Acquiring...");
+    //Take down values and store in an array
+    for (int i = 0; i<100; i++) {
+      volts = analogRead(Sen);
+      array[i] = volts*3.3/4095;
+      //Serial.println(array[i]);
+      delay(10);
+    }
+    //Sort the array to find the mode
+    sortArray(array, 100);
+    //Determine the mode
+    for (int i = 1; i<100; i++) {
+      if (array[i] == number){
+        count++;
+      }
+      else {
+        if (count > countMode){
+          countMode = count;
+          Mode = number;
+        }
+        count = 1;
+        number = array[i];
+      }
+    }
+    Serial.println("Sample mode voltage: ");
+    Serial.println(Mode);
+  }
+  
+  /*float volts1 = analogRead(Sen);
+  delay(5);
+  float voltavg = ((volts1))*5/4095;
+  Serial.println(voltavg);*/
+}
